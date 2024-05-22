87a570726e00ea858389e6d2c8dbf31aec7292ad
Amine Sekhri <Amine.skr16@gmail.com>
2024-05-22 20:59:09 +0100

    first commit

---
+++ Exercice.js
@@ -0,0 +1,90 @@
+// Exercise 1: Basic Syntax and Variables
+// Create a variable named myName and assign it your name as a string.
+// Create a variable named age and assign it your age as a number.
+// Create a variable named isStudent and assign it a boolean value indicating whether you are a student or not.
+// Print all three variables to the console.
+// Solution
+// let MyName = "Amine"
+// let age = 33
+// let IsStudent = true
+// console.log(MyName,age,IsStudent)
+
+//**************************************************************************************************** */
+
+// Exercise 2: Data Types
+// Create an array named favoriteFoods and populate it with three of your favorite foods.
+// Create an object named person with properties for name, age, and hobbies (an array of your hobbies).
+// Print the person object and favoriteFoods array to the console.
+
+// Solution
+// let favoriteFoods = ["rice","meat","chicken"]
+// let person = {
+//     name:"Amine",
+//     age:33,
+//     hobbies:["fishing","camping","football"]
+    
+// }
+// console.log(person)
+// console.log(favoriteFoods)
+//***************************************************************************************************** */
+
+// Exercise 3: Conditions
+// Write a function named checkAge that takes an age as a parameter and prints "Adult" if the age is 18 or older, and "Minor" if the age is under 18.
+
+// solution
+// function checkAge(age) {
+//     if(age>=18){
+//         console.log('Adult')
+//     }
+//     else{
+//         console.log("Minor")
+//     }
+// }
+
+// checkAge(40)
+
+//************************************************************************************************** */
+
+// Exercise 4: Loops
+// Write a for loop that prints the numbers from 1 to 10.
+// Write a while loop that prints the numbers from 10 to 1.
+
+// Solution
+
+// for (let i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// let number = 10
+// while(number>=1){
+//     console.log(number)
+//     number=number-1
+// }
+
+//************************************************************************************************ */
+
+// Exercise 5: Functions
+// Write a function named add that takes two numbers as parameters and returns their sum.
+// Write a function named greet that takes a name as a parameter and prints a greeting message.
+
+// solution
+
+// function add(num1,num2){
+//     return num1+num2
+// }
+
+// function greet(name){
+//     console.log("hello "+name)
+// }
+
+// greet("amine")
+// let result = add(20,10)
+// console.log(result)
+
+//************************************************************************************************* */
+
+// Exercise 6: Combining Everything
+// Create a function named createPerson that takes name, age, and hobbies as parameters and returns an object with those properties.
+// Create a function named describePerson that takes a person object and prints a description of the person (descriping the person's name, age and hobbies).
+
+// Solution
